abf1499e237a28ecf87e5a92605a86be3c90ad40
legionDroid <brunolegion@hotmail.com>
2019-12-08 04:09:23 -0300

    add modulos e Lazy Loanding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,25 @@ import { Routes, RouterModule } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 import { NgifNgforComponent } from './ngif-ngfor/ngif-ngfor.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
+  {
+    path: "",
+    component: NgifNgforComponent 
+  },
+  {
+    path: 'lazy',
+    loadChildren: () =>
+      import('./modules/lazyloading/lazyloading.module').then(
+        module => module.LazyloadingModule
+      )
+  },
+
   { path: 'primeiro-componente', component: DataBindingComponent },
   { path: 'segundo-componente', component: SegundoComponenteComponent },
   { path: 'ngIfngFor', component: NgifNgforComponent }
-
-  
-
 ];
 
 @NgModule({
---
+++ src/app/modules/lazyloading/lazyloading-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from 'src/app/componentes/footer/footer.component';
+import { HeaderComponent } from 'src/app/componentes/header/header.component';
+import { HomelazyComponent } from 'src/app/componentes/homelazy/homelazy.component';
+
+
+const routes: Routes = [
+  {path: "", 
+  component: HomelazyComponent,
+  children: [
+    {
+      path: "header",
+      component: HeaderComponent
+    },
+    {
+      path: "footer",
+      component: FooterComponent
+    }
+  ]
+}
+];
+
+@NgModule({
+  declarations: [FooterComponent, HeaderComponent, HomelazyComponent],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LazyloadingRoutingModule { }
